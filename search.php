6d19507eebd995f67c012b2a789d3ba47a1fdb25
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2023-12-09 13:29:33 -0500

    Update search.php

--- search.php
+++ search.php
@@ -17,8 +17,11 @@
         <textarea name="search_query" placeholder="Search for a specific user by typing the username" cols="50"></textarea><br>
         <input type="submit" value="Search">
         <br><br>
-        <textarea name="table_query" placeholder="Type users_new to see the entire specified table" cols="50"></textarea><br>
+        <textarea name="table_query" placeholder="Type users_new to see the entire username table" cols="50"></textarea><br>
         <input type="submit" value="Search Table">
+        <br><br>
+        <textarea name="property_query" placeholder="Type properties to retrieve all property listings" cols="50"></textarea><br>
+        <input type="submit" value="Search Properties">
     </form>
     <p><a href='index.html'>Return to homepage</a></p>
 </body>
